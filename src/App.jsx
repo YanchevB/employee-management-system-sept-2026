6438795119d163a8e3ed1bfbf37265a96b61ee0b
Boyan Yanchev <byanchev6@gmail.com>
2026-09-22 21:45:41 +0300

    chore: create page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
+import './styles.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Pagination from './components/Pagination'
+import UserList from './components/UserList'
+import UserSearch from './components/UserSearch'
 
 function App() {
 
-  return (
-    <>
-        <h1>Hello world</h1>
-    </>
-  )
+    return (
+        <>
+            <Header />
+
+            <main className="main">
+                <section className="card users-container">
+                    <UserSearch />
+
+                    <UserList />
+
+                    {/* New user button  */}
+                    <button className="btn-add btn">Add new user</button>
+
+                    <Pagination />
+                </section>
+            </main>
+            
+            <Footer />
+        </>
+
+    )
 }
 
 export default App
